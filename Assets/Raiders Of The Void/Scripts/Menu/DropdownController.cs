55a35ff9faef8d782c13355de67f949d8f728e27
kcrash201 <10089172@student.sae.edu.au>
2019-03-21 15:59:11 +0800

    Dropdown Menu Toggle

---
+++ Assets/Raiders Of The Void/Scripts/Menu/DropdownController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropdownController : MonoBehaviour
+{
+
+    public GameObject menu;
+    public bool isVisable;
+
+    // Update is called once per frame
+    public void Dropdown()
+    {
+        isVisable = !isVisable;
+        menu.SetActive(isVisable);
+    }
+}
