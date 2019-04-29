75747e723e3db863d68b165533235d1523e284d3
kcrash201 <10089172@student.sae.edu.au>
2019-04-29 14:09:13 +0800

    Menu/Credit Button Audio
    Inefficent if jumping between scenes too often as it doesnt delete the old controller yet

---
+++ Assets/Scripts/Menu/ButtonSFX.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSFX : MonoBehaviour
+{
+    public AudioSource myAudio;
+    public AudioClip hoverAudio;
+    public AudioClip clickAudio;
+    float lifetime = 3.0f;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+        public void HoverSound()
+    {
+        myAudio.PlayOneShot(hoverAudio);
+    }
+    public void ClickSound()
+    {
+        myAudio.PlayOneShot(clickAudio);
+    }
+    private void DestroyAudio()
+    {
+        Destroy(gameObject, lifetime);
+    }
+}
